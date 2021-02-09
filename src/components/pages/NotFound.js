8b9f0a57dc33e2cfc3569c26dcc649c2cb3c46b5
Solomiia <ssahala@Solomiias-MacBook-Pro.local>
2021-02-09 17:44:59 +0200

    added Home and NotFound component, made last changes, ready project

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NotFound = () => {
+    return (
+        <div className="lead">
+            <h1>The page is not found</h1>
+        </div>
+    )
+}
+
+export default NotFound;
